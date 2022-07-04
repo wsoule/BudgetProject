b78a2b4031ea40b6ff0f6edbc9e7e56ca8cec46e
Wyat Soule <wsoule679@gmail.com>
2022-07-04 19:44:17 -0400

    add subscritions to value changes and link range bar to percent input

--- Budgets/src/app/house-cost/house-cost.component.ts
+++ Budgets/src/app/house-cost/house-cost.component.ts
@@ -9,26 +9,55 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./house-cost.component.css']
 })
 export class HouseCostComponent implements OnInit {
+  public static defaultCost = 100000;
+  public static defaultDownPercent = 20;
+  public static defaultDownCost = HouseCostComponent.defaultCost * HouseCostComponent.defaultDownPercent / 100;
   costForm: FormGroup<TotHousePrice>;
-  //how to have this empty?
-  // totCost: totHousePrice ={
-  //   cost: 100000
-  // };
+
   constructor() {
     this.costForm = new FormGroup({
       cost: new FormControl<number | null>(
-        null,
+        HouseCostComponent.defaultCost,
         [
           Validators.required,
           Validators.min(5),
           Validators.max(10 ** 21)
+        ]
+      ),
+      dPayCost: new FormControl<number | null>(
+        HouseCostComponent.defaultDownCost,
+        [
+          // Validators.required,
+          Validators.min(0)
+        ]
+      ),
+      dPayPercent: new FormControl<number | null>(
+        HouseCostComponent.defaultDownPercent, 
+        [
+          // Validators.required,
+          Validators.min(0),
+          Validators.max(100)
         ]
       )
     });
    }
 
   ngOnInit(): void {
-    
+    this.costForm.get('cost')?.valueChanges.subscribe((cost) => {
+      const { dPayPercent } = this.costForm.value;
+      this.costForm.patchValue({
+        dPayCost: this.toDollar((cost ?? 0) * (dPayPercent ?? 0) / 100)
+      });
+    });
+    this.costForm.get('dPayPercent')?.valueChanges.subscribe((dPayPercent) => {
+      const { cost } = this.costForm.value;
+      this.costForm.patchValue({
+        dPayCost: this.toDollar((cost ?? 0) * (dPayPercent ?? 0) / 100),
+        dPayPercent
+      }, {
+        emitEvent: false
+      });
+    })
   }
 
   getControl(formKey: string): FormControl<any> {
@@ -39,5 +68,7 @@ export class HouseCostComponent implements OnInit {
     const control = this.costForm.get(formKey) as FormControl<any>;
     return (control.errors && control.errors[errorKey]) != null;
   }
-
+  toDollar(number: number):number {
+    return Math.floor(number * 100)/100
+  }
 }
--- Budgets/src/app/housePrice.ts
+++ Budgets/src/app/housePrice.ts
@@ -2,4 +2,6 @@ import { FormControl } from '@angular/forms';
 
 export interface TotHousePrice{
     cost: FormControl<number | null>;
+    dPayCost: FormControl<number | null>;
+    dPayPercent: FormControl<number | null>;
 }
